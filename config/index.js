8b60d2c09ff44bf5b66f6150b1a1d7a736f4bb17
Diego Santiesteban <gatodeveloper@gmail.com>
2019-11-28 02:49:46 -0500

    Added log

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = (function(){
 
-  var config = {
+  var initConfig = {
     db: {
       connectionString: 'mongodb+srv://G4t04dm1n:W0TGPRf7ekUbE44O@cluster0-olsgc.mongodb.net/test?retryWrites=true&w=majority'
     },
@@ -8,6 +8,7 @@ module.exports = (function(){
   };
 
   var envConfig = require('./env/' + (process.env.NODE_ENV ? process.env.NODE_ENV : 'development'));
-  var config = Object.assign(config, envConfig);
+  var config = Object.assign(initConfig, envConfig);
+  console.log(config);
   return config;
 })();
