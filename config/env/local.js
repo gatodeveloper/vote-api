8848411f9ed9c8a569aec3fef7fce8aaf525b9ee
Diego Santiesteban <gatodeveloper@gmail.com>
2019-11-28 02:45:13 -0500

    Updated envs data

--- config/env/local.js
+++ config/env/local.js
@@ -1,1 +1,6 @@
-module.exports = {}
+module.exports = {
+	db: {
+      connectionString: 'mongodb+srv://G4t04dm1n:W0TGPRf7ekUbE44O@cluster0-olsgc.mongodb.net/test?retryWrites=true&w=majority'
+    },
+    JWT_KEY: 'GatoDeveloper'
+}
--- config/env/production.js
+++ config/env/production.js
@@ -1,3 +1,6 @@
 module.exports = {
-	
+	db: {
+      connectionString: 'mongodb+srv://G4t04dm1n:W0TGPRf7ekUbE44O@cluster0-olsgc.mongodb.net/test?retryWrites=true&w=majority'
+    },
+    JWT_KEY: 'GatoDeveloper'
 }
